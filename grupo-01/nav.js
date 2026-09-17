986bf1b03874cf513de5c0eb68edbdd7f7447093
janisepulveda <janis.sepulveda@mail.udp.cl>
2026-09-17 16:36:45 -0300

    agregar grupos

--- grupo-01/nav.js
+++
@@ -1,7 +0,0 @@
-const signalButton = document.querySelector("[data-signal]");
-const status = document.querySelector("[data-status]");
-
-signalButton?.addEventListener("click", () => {
-	status.textContent = "Señal activa";
-	signalButton.innerHTML = 'Señal activada <span>✓</span>';
-});
